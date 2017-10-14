9c7522a947561274f5a3f87f9d3ce31487a5790c
christophebedard <chrisvolkoff@gmail.com>
2017-10-13 22:15:31 -0400

    Add basic dh_to_tf node

---
+++ src/dh_to_tf.cpp
@@ -0,0 +1,44 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/Matrix3x3.h>
+#include <std_msgs/Float64.h>
+
+static const std::string NODE_NAME = "dh_to_tf";
+
+std::string child_frame, frame, angle_topic;
+double d, a, alpha;
+
+void angleCallback(const std_msgs::Float64& msg){
+    double theta = msg->data;
+
+    tf::Transform transform;
+    tf::Matrix3x3 trans(cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha),
+                    sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha),
+                             0,             sin(alpha),             cos(alpha));
+    tf::Vector3 rot(a*cos(theta),
+                    a*sin(theta),
+                               d);
+    tf::Transform transform(rot, trans);
+
+    static tf::TransformBroadcaster br;
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), child_frame, frame));
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, NODE_NAME);
+    ros::NodeHandle n;
+
+    ros::NodeHandle n_p("~");
+    n_p.getParam("child_frame", child_frame);
+    n_p.getParam("frame", frame);
+    n_p.getParam("angle_topic", angle_topic);
+    n_p.getParam("d", d);
+    n_p.getParam("a", a);
+    n_p.getParam("alpha", alpha);
+
+    ros::Subscriber angle_sub = node.subscribe(angle_topic, 10, &angleCallback);
+    
+    ros::spin();
+    
+    return 0;
+}
