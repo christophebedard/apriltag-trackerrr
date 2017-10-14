523c4bc0a01131c895f8ab9a48cb0423bbd80c88
christophebedard <chrisvolkoff@gmail.com>
2017-10-14 17:13:33 -0400

    Use yaml file for DH matrix

--- src/dh_to_tf.cpp
+++ src/dh_to_tf.cpp
@@ -1,14 +1,22 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <std_msgs/Float64.h>
+#include <string>
 
 static const std::string NODE_NAME = "dh_to_tf";
+static const int COL_OFFSET = 0;
+static const int COL_D      = 1;
+static const int COL_A      = 2;
+static const int COL_ALPHA  = 3;
+static const int COL_TOTAL  = 4;
 
-std::string child_frame, frame, angle_topic;
-double d, a, alpha;
+int joint_id;
+std::vector<double> dh_matrix;
+std::string frame_prefix, angle_topic_prefix, child_frame, frame, angle_topic;
+double offset, d, a, alpha;
 
 void angleCallback(const std_msgs::Float64::ConstPtr& msg){
-    double theta = msg->data;
+    double theta = offset + msg->data;
 
     tf::Matrix3x3 rot(cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha),
                       sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha),
@@ -27,12 +35,19 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
 
     ros::NodeHandle n_p("~");
-    n_p.getParam("child_frame", child_frame);
-    n_p.getParam("frame", frame);
-    n_p.getParam("angle_topic", angle_topic);
-    n_p.getParam("d", d);
-    n_p.getParam("a", a);
-    n_p.getParam("alpha", alpha);
+    n_p.getParam("joint_id", joint_id);
+    n_p.getParam("/frame_prefix", frame_prefix);
+    n_p.getParam("/angle_topic_prefix", angle_topic_prefix);
+    n_p.getParam("/dh_matrix", dh_matrix);
+    
+    child_frame = frame_prefix + std::to_string(joint_id - 1);
+    frame       = frame_prefix + std::to_string(joint_id);
+    angle_topic = angle_topic_prefix + std::to_string(joint_id);
+
+    offset = dh_matrix[COL_TOTAL * (joint_id - 1) + COL_OFFSET];
+    d      = dh_matrix[COL_TOTAL * (joint_id - 1) + COL_D];
+    a      = dh_matrix[COL_TOTAL * (joint_id - 1) + COL_A];
+    alpha  = dh_matrix[COL_TOTAL * (joint_id - 1) + COL_ALPHA];
 
     ros::Subscriber angle_sub = n.subscribe(angle_topic, 10, &angleCallback);
     
