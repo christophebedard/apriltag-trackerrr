ea4b503ff980a722e7122fdf08d18f28f7b41d31
christophebedard <chrisvolkoff@gmail.com>
2017-10-16 22:24:37 -0400

    Add callback for detected tags and basic visualisation of tracking error

--- include/apriltag_trackerrr/tagtracker.h
+++ include/apriltag_trackerrr/tagtracker.h
@@ -1,30 +1,90 @@
 #ifndef TAG_TRACKER_H
 #define TAG_TRACKER_H
 
+#include "apriltags_ros/AprilTagDetectionArray.h"
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <image_geometry/pinhole_camera_model.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+static const double LOOP_RATE = 10.0;
+static const std::string CAMERA_NAMESPACE = "camera";
+static const std::string IMAGE_RECT_TOPIC_NAME = "image_rect";
+static const std::string TAG_DETECTIONS_TOPIC_NAME = "tag_detections";
+static const int TARGET_TAG_ID = 27;
+
 class TagTracker
 {
-    private:
-        ros::NodeHandle n_;
-        image_transport::ImageTransport it_;
-        image_transport::Subscriber image_sub_;
-        image_transport::Publisher image_pub_;
+    public:
+        /*
+         * Constructor
+         */
+        TagTracker(ros::NodeHandle& n);
+        /*
+         * Destructor
+         */
+        ~TagTracker();
 
-        void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+        /*
+         * ROS spin. Called only once (by node); contains ROS while loop
+         */
+        void spin();
 
 
     protected:
+        /*===========================
+         * Update
+         *===========================*/
+        /*
+         * Update; called every spinOnce()
+         */
+        void update();
+
+        /*
+         * ROS spin once, called on every loop
+         */
+        void spinOnce();
 
 
-    public:
-        TagTracker(ros::NodeHandle& n);
-        ~TagTracker();
+    private:
+        ros::NodeHandle n_;
+        bool is_running_slowly_;
+        double loop_hz_;
+
+        std::vector<apriltags_ros::AprilTagDetection> detected_tags_;
+
+        image_transport::ImageTransport it_;
+        image_geometry::PinholeCameraModel cam_model_;
+        /*===========================
+         * Subscribers
+         *===========================*/
+        /* Camera image subscriber */
+        image_transport::CameraSubscriber image_sub_;
+        /* Apriltag detection subscriber */
+        ros::Subscriber tag_detect_sub_;
+
+        /*===========================
+         * Publishers
+         *===========================*/
+        /* Publisher for image with tracking error info */
+        image_transport::Publisher track_image_pub_;
+
+        /*===========================
+         * Callbacks
+         *===========================*/
+        /*
+         * Callback class method for camera image
+         */
+        void imageCallback(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info);
+
+        /*
+         * Callback class method for apriltag detection
+         */
+        void tagPositionCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr& msg);
+
 
 };
 
--- src/tagtracker.cpp
+++ src/tagtracker.cpp
@@ -1,10 +1,14 @@
 #include "apriltag_trackerrr/tagtracker.h"
 
 TagTracker::TagTracker(ros::NodeHandle& n)
-    : n_(n), it_(n)
+    : n_(n), it_(n), loop_hz_(LOOP_RATE)
 {
-    image_sub_ = it_.subscribe("/camera/image_rect", 1, &TagTracker::imageCallback, this);
-    image_pub_ = it_.advertise("/camera/tracking_error", 1);
+    // setup subscribers
+    image_sub_ = it_.subscribeCamera("/"+CAMERA_NAMESPACE+"/"+IMAGE_RECT_TOPIC_NAME, 100, &TagTracker::imageCallback, this);
+    tag_detect_sub_ = n_.subscribe("/"+CAMERA_NAMESPACE+"/"+TAG_DETECTIONS_TOPIC_NAME, 100, &TagTracker::tagPositionCallback, this);
+
+    // setup publishers
+    track_image_pub_ = it_.advertise("/"+CAMERA_NAMESPACE+"/"+"tracking_error", 1);
 }
 
 TagTracker::~TagTracker()
@@ -12,9 +16,13 @@ TagTracker::~TagTracker()
 
 }
 
-void TagTracker::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+/*===========================
+ * Callbacks
+ *===========================*/
+
+void TagTracker::imageCallback(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info) {
+    // convert image to opencv image type
     cv_bridge::CvImagePtr cv_ptr;
-    
     try
     {
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -24,14 +32,73 @@ void TagTracker::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
-    
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60) {
-        cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+    // update camera model
+    cam_model_.fromCameraInfo(cam_info);
+
+    int middle_x = cv_ptr->image.cols / 2;
+    int middle_y = cv_ptr->image.rows / 2;
+    cv::Point2d middle(middle_x, middle_y);
+    //cv::circle(cv_ptr->image, cv::Point(middle_x, middle_y), 50, CV_RGB(255,0,0));
+
+    // draw
+    for (apriltags_ros::AprilTagDetection tag : detected_tags_) {
+        cv::Point3d pt_cv(tag.pose.pose.position.x, tag.pose.pose.position.y, tag.pose.pose.position.z);
+        cv::Point2d uv = cam_model_.project3dToPixel(pt_cv);
+        cv::arrowedLine(cv_ptr->image, middle, uv, CV_RGB(255,0,0));
     }
 
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+
+    // output modified video stream
+    track_image_pub_.publish(cv_ptr->toImageMsg());
+}
+
+void TagTracker::tagPositionCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr& msg) {
+    // update vector
+    detected_tags_ = msg->detections;
+}
+
+/*===========================
+ * Update
+ *===========================*/
+
+void TagTracker::update() {
+    if (!detected_tags_.empty()) {
+        // not empty; found tag(s)
+        ROS_INFO_STREAM("found tag(s) :");
+        int size = detected_tags_.size();
+        ROS_INFO_STREAM(size);
+
+        for (apriltags_ros::AprilTagDetection tag : detected_tags_) {
+            ROS_INFO_STREAM(tag.id);
+        }
+
+    } else {
+        // empty; didn't find tag
+        //ROS_INFO_STREAM("DID NOT find tag");
+    }
+}
+
+void TagTracker::spinOnce()
+{
+    update();
+    ros::spinOnce();
+}
+
+void TagTracker::spin()
+{
+    ros::Rate rate(loop_hz_);
+    
+    while (ros::ok())
+    {
+        spinOnce();
+        
+        is_running_slowly_ = !rate.sleep();
+        if (is_running_slowly_)
+        {
+            ROS_WARN("[TAGTRACKER] Loop running slowly.");
+        }
+    }
 }
 
 // ---------------------------
@@ -47,7 +114,14 @@ int main(int argc, char** argv)
     
     TagTracker tag_tracker(n);
     
-    ros::spin();
-    
+    try
+    {
+        tag_tracker.spin();
+    }
+    catch (std::runtime_error& e)
+    {
+        ROS_FATAL_STREAM("[TAGTRACKER] Runtime error: " << e.what());
+        return 1;
+    }    
     return 0;
 }
